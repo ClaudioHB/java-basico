c4b923e2661510a53eca19cc694c105a74f2856b
Claudio Bispo <chbsmota@gmail.com>
2026-09-26 10:34:26 -0300

    Exemplos e exercicios para tipos e variaveis

---
+++ Meu-Primeiro-Projeto-Java/src/Tipos-e-Variaveis/TiposeVariaves.java
@@ -0,0 +1,9 @@
+public class TiposeVariaves {
+
+    int NumeroInteiros = 25;
+    double NumeroDecimalAltaPrecisao = 19.99;
+    float NumeroDecimalSimples = 1.75f;
+    boolean ValoresLogicos = true;
+    char UmUnicoCaractere = 'A';
+    String SequenciadeCaracteres = "Textos ou palavras";
+}
